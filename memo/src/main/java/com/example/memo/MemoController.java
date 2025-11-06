84e0889abeaaa6f51e2e2914f191aaf81d95c4af
lumiera78 <yoshikiwatari0807@gmail.com>
2025-11-06 17:38:28 +0900

    update

--- memo/src/main/java/com/example/memo/MemoController.java
+++ memo/src/main/java/com/example/memo/MemoController.java
@@ -1,12 +1,21 @@
 package com.example.memo;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemoController {
+    private final MemoService memoService;
+
+    private MemoController(MemoService memoService){
+        this.memoService = memoService;
+    }
+
     @GetMapping("/")
-    public String getMemo(){
-    return "index";
+    public String getMemo(Model model){
+        String message = memoService.getMessage();
+        model.addAttribute("message", message);
+        return "index";
     }
 }
